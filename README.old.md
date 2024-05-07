332cccaca45d2b37ca74d38e663ec04fcf0106d0
Adilet Zainidinov <adiletzainidinov22@gmail.com>
2024-05-08 02:06:20 +0600

    hw-11.

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# react-todo-2023
-A React todo app 2023!
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,15 +2,21 @@ import { useContext } from 'react';
 import Todo from './Todo';
 import { TodoContext } from '../context/TodoContext';
 const TodoList = () => {
-    const [ todos ] = useContext( TodoContext );
-    return(
-        1 <= todos.length ? todos.map( ( item ) => {
-            return(
-                <Todo key={ item.id } id={ item.id } title={ item.title } completed={ item.completed } />
-            );
-        } ) : ( <h4>No todo found. Please add some...</h4> )
-    );
-}
+  const [todos] = useContext(TodoContext);
+  return 1 <= todos.length ? (
+    todos.map((item) => {
+      return (
+        <Todo
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          completed={item.completed}
+        />
+      );
+    })
+  ) : (
+    <h4 style={{ marginTop: '20px',marginRight: '100px', fontSize: '18px' }}>Нет задачи,пожалуйста добавьте</h4>
+  );
+};
 
 export default TodoList;
-
